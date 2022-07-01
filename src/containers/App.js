8a185067f0caf5858d4bc5b411b35c03825b5c7c
Luciano Staniszewski <luciano.stani97@gmail.com>
2022-07-01 13:24:17 -0300

    added secure to http fetch

--- src/containers/App.js
+++ src/containers/App.js
@@ -17,7 +17,7 @@ function App() {
   }
   function onSearch(ciudad) {
     //Llamado a la API del clima
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`)
       .then(r => r.json())
       .then((recurso) => {
         if(recurso.main !== undefined){
